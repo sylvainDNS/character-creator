239e1f76b6921e14025090d83bc29662ce28a7a5
Sylvain DNS <sylvain.denyse@gmail.com>
2021-05-02 17:30:53 +0200

    exercise 4

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
+import PerspectiveBar from './components/PerspectiveBar/PerspectiveBar';
 
 function App() {
   return (
     <>
+      <PerspectiveBar/>
       <CharacterEditor />
       <Footer />
     </>
---
+++ src/components/PerspectiveBar/PerspectiveBar.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import styles from './PerspectiveBar.module.css'
+
+const PerspectiveBar = () => <div className={styles['perspective-bar']}></div>
+
+export default PerspectiveBar
